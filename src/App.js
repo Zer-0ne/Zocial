1804d2341a4c90bf50493c8a763fb5d008b4d375
Zer-0ne <sahilkhan8294799@gmail.com>
2023-04-13 22:07:44 +0530

    Zocial

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+
+function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
+
+  return (
+    <div className="app">
+      <BrowserRouter basename="/Zocial">
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route 
+              exact 
+              path="/" 
+              element={<LoginPage />} 
+            />
+            <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
